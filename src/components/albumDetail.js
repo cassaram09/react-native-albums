99f77a52c92458967c322f08bdc8d3a4f22ae1d1
cassaram09 <cassaram09@gmail.com>
2018-01-11 07:52:49 -0500

    add button with links and text

--- src/components/albumDetail.js
+++ src/components/albumDetail.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, Linking } from 'react-native';
 import Card from './card';
 import CardSection from './cardSection';
+import Button from './button';
 
 const AlbumDetail = ({ album }) => {
-  const { title, artist, thumbnail_image, image } = album;
-  const { thumbnailStyle, headerContentStyle, thumbnailContainerStyle, headerTextStyle, imageStyle } = styles; 
+  const { title, artist, thumbnail_image, image, url } = album;
+  const { 
+    thumbnailStyle, 
+    headerContentStyle, 
+    thumbnailContainerStyle, 
+    headerTextStyle, 
+    imageStyle 
+  } = styles; 
+
   return (
     <Card>
       <CardSection>
@@ -20,11 +28,18 @@ const AlbumDetail = ({ album }) => {
           <Text>{artist}</Text>
         </View>
       </CardSection>
+
       <CardSection>
-          <Image
-            style={imageStyle} 
-            source={{ uri: image }} 
-          />
+        <Image
+          style={imageStyle} 
+          source={{ uri: image }} 
+        />
+      </CardSection>
+
+      <CardSection>
+        <Button onPress={() => Linking.openURL(url)} >
+          Buy Now
+        </Button>
       </CardSection>
     </Card>
   );
